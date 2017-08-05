27e0483a5b4a765a3d7a1568185be232149ecee9
Todd Stellanova <todd@droneflow.com>
2017-08-05 21:10:31 +0000

    Sweeter fading

--- rusty_pi_butt/src/main.rs
+++ rusty_pi_butt/src/main.rs
@@ -1,7 +1,9 @@
 extern crate rust_pigpio;
 
 use std::thread::sleep;
+use std::time::Duration;
 
+// We use pigpio to generate and monitor signals on GPIO pins
 use rust_pigpio::*;
 use rust_pigpio::pwm::*;
 
@@ -14,28 +16,31 @@ const FADE_STEPS: u32 = 25;
 const FADE_STEP_VAL: u32 = (PWM_FULL_RANGE / FADE_STEPS);
 
 fn led_on(duration_secs: u64) {
+  // the LED safety switch I'm using is already tied to +VDC: set GPIO pin low to turn on
   write(LED_GPIO_PIN, OFF).unwrap();
-  println!("Bright..");
-  sleep(std::time::Duration::from_secs(duration_secs));
+  println!("Bright...");
+  sleep(Duration::from_secs(duration_secs));
 }
 
 fn led_off(duration_secs: u64) {
   write(LED_GPIO_PIN, ON).unwrap();
   println!("Dark...");
-  sleep(std::time::Duration::from_secs(duration_secs));
+  sleep(Duration::from_secs(duration_secs));
 }
 
 fn fade_led_down(pin: u32 ) {
-    set_pwm_frequency(pin, LED_PWM_FREQ_HZ).unwrap(); 
-    set_pwm_range(pin, PWM_FULL_RANGE).unwrap(); // Set range to 1000. 1 range = 2 us;
+  set_pwm_frequency(pin, LED_PWM_FREQ_HZ).unwrap(); 
+  set_pwm_range(pin, PWM_FULL_RANGE).unwrap(); // Set range to 1000. 1 range = 2 us;
 
+  println!("Fade down...");
   for x in 0..FADE_STEPS{
     let duty_cycle = x * FADE_STEP_VAL;
     //println!("duty_cycle: {}", duty_cycle);
     
     pwm(pin, duty_cycle).unwrap(); 
+    //TODO hardware_pwm doesn't seem to work.  Check docs, impl
     //hardware_pwm(pin, LED_PWM_FREQ_HZ, duty_cycle).unwrap();
-    sleep(std::time::Duration::from_millis(FADE_STEP_DELAY_MS))
+    sleep(Duration::from_millis(FADE_STEP_DELAY_MS))
   }
 }
 
@@ -43,16 +48,28 @@ fn fade_led_up(pin: u32) {
   set_pwm_frequency(pin, LED_PWM_FREQ_HZ).unwrap();
   set_pwm_range(pin, PWM_FULL_RANGE).unwrap(); // Set range to 1000. 1 range = 2 us;
 
+  println!("Fade up...");
+
   for x in 0..FADE_STEPS{
     let duty_cycle = PWM_FULL_RANGE - x * FADE_STEP_VAL;
     //println!("duty_cycle: {}", duty_cycle);
 
     pwm(pin, duty_cycle).unwrap();  
-    //hardware_pwm(pin, LED_PWM_FREQ_HZ, duty_cycle).unwrap();
-    sleep(std::time::Duration::from_millis(FADE_STEP_DELAY_MS))
+    //TODO hardware_pwm doesn't seem to work.  Check docs, impl
+
+    //let pwm_duty = (duty_cycle / PWM_FULL_RANGE) * 1000000;
+    //hardware_pwm(pin, LED_PWM_FREQ_HZ, pwm_duty).unwrap();
+    sleep(Duration::from_millis(FADE_STEP_DELAY_MS))
   }
 
 }
+
+
+/*
+   gpio: see description
+PWMfreq: 0 (off) or 1-125000000 (125M)
+PWMduty: 0 (off) to 1000000 (1M)(fully on)
+*/
 
 fn led_fade_cycle(count: u32) {
   for x in 0..count {
