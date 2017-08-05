d0d96ebf39067f2c8752ba5714dac5dc92edff4e
Todd Stellanova <todd@droneflow.com>
2017-08-05 15:30:42 +0000

    Add some basic pwm calls

--- rusty_pi_butt/main.rs
+++ rusty_pi_butt/main.rs
@@ -1,4 +1,11 @@
 extern crate rust-pigpio;
+
+
+const LED_GPIO_PIN: u32 = 18;
+
 fn main() {
-  println!("Hello World!");
+  // set the pwm value
+  hardware_pwm(LED_GPIO_PIN, 1000, 50);
+  //hardware_pwm(gpio: u32, frequency: u32, dutycycle: u32)
+  println!("Yello!");
 }
